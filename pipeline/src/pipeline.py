31a525ec5310f3ecbe633bba5c143ba062fb48d2
jackma-00 <jacopo.maragna1@gmail.com>
2022-12-21 15:29:58 +0100

    CMD fixed and first version of pipeline added

--- pipeline/src/pipeline.py
+++ pipeline/src/pipeline.py
@@ -1,5 +1,5 @@
 import kfp
-from kfp.v2.dsl import ContainerOp, Dataset, Input, pipeline, Output
+from kfp.dsl import ContainerOp, pipeline, InputArgumentPath
 from kfp import compiler
 
 # Component's images
@@ -7,34 +7,34 @@ LOAD_DATASET_IMAGE = 'jackma00/load-dataset:latest'
 NORMALIZE_DATASET_IMAGE = 'jackma00/normalize_dataset:latest'
 
 # Building components from images
-def load_dataset_component(raw_dataset: Output[Dataset]):
+def load_dataset_component():
     return ContainerOp(
         name='Load Data',
         image=LOAD_DATASET_IMAGE,
-        command=[], # The entry point has already been provided with the Dockerfile
-        args=[], # None
-        file_outputs={raw_dataset.path: 'app/raw_dataset.csv'})
+        #command=[], # The entry point has already been provided with the Dockerfile
+        arguments=[], # None
+        file_outputs={'raw_dataset': 'app/raw_dataset.csv'})
 
-def normalize_dataset_component(raw_dataset: Input[Dataset], 
-                                normalized_dataset: Output[Dataset]):
+def normalize_dataset_component(raw_dataset):
     return ContainerOp(
         name='Normalize Data',
         image=NORMALIZE_DATASET_IMAGE,
-        command=[], # The entry point has already been provided with the Dockerfile
-        args=['--raw_dataset', raw_dataset.path],
-        file_outputs={normalized_dataset.path: 'app/normalized_dataset.csv'})
+        #command=[], # The entry point has already been provided with the Dockerfile
+        arguments=['--raw_dataset', raw_dataset],
+        file_outputs={'normalized_dataset': 'app/normalized_dataset.csv'})
 
 
 # Define the pipeline
 @pipeline(
     name='data-pipeline',
-    description='This simple data pipeline wants to the the custom created components')
+    description='This simple data pipeline wants to test the custom created components')
 def data_pipeline():
     load_dataset_task = load_dataset_component()
 
     normalize_dataset_task = normalize_dataset_component(
-        raw_dataset=load_dataset_task.outputs['raw_dataset'])
+        raw_dataset=InputArgumentPath(load_dataset_task.file_outputs['raw_dataset'])).after(load_dataset_task)
 
 # Compile the pipeline
-cmplr = compiler.Compiler(mode=kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE)
-cmplr.compile(pipeline_func=data_pipeline, package_path='data_pipeline.yaml')
+compiler.Compiler().compile(
+    pipeline_func=data_pipeline,
+    package_path='data_pipeline.yaml')
