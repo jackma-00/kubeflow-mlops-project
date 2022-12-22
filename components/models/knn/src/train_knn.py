623d44105da0e2087f6eefe23bef2d92c4fc026d
jackma-00 <jacopo.maragna1@gmail.com>
2022-12-22 12:23:13 +0100

    train component added

---
+++ components/models/knn/src/train_knn.py
@@ -0,0 +1,42 @@
+import argparse
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+N_NEIGHBORS = 6
+
+def train_knn(normalized_dataset):
+    """The function trains a knn model."""
+
+    # Load normalized dataset
+    df = pd.read_csv(normalized_dataset)
+
+    y = df.pop('Labels')
+    X = df
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    clf = KNeighborsClassifier(n_neighbors=N_NEIGHBORS)
+    clf.fit(X_train, y_train)
+    
+    return clf
+
+def save_model(model):
+    """The function serializes a model."""
+    
+    with open('knn.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+if __name__ == '__main__':
+
+    # Parsing arguments 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--normalized_dataset')
+    args = parser.parse_args()
+
+    # Train model
+    knn = train_knn(args.normalized_dataset)
+
+    # Save model
+    save_model(knn)
